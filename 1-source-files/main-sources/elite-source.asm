4c521f77ad7017d7acf7ad79dd585d165cb26691
Mark Moxon <mark@moxon.net>
2026-05-06 10:22:20 +0100

    Fix layout error in LL51 header, incorrect text cursor comments

--- 1-source-files/main-sources/elite-source.asm
+++ 1-source-files/main-sources/elite-source.asm
@@ -34401,9 +34401,7 @@ ENDMACRO
 ; When called from part 6 of LL9, XX12 contains the vector [x y z] of the vertex
 ; we're analysing, and XX16 contains the transposed orientation vectors with
 ; each of them containing the x, y and z elements of the original vectors, so it
-; ------------------------------------------------------------------------------
-;
-; Returns:
+; returns:
 ;
 ;   [ x ]   [ sidev_x ]         [ x ]   [ sidev_y ]         [ x ]   [ sidev_z ]
 ;   [ y ] . [ roofv_x ]         [ y ] . [ roofv_y ]         [ y ] . [ roofv_z ]
@@ -39746,7 +39744,7 @@ ENDMACRO
  LDA QQ11               ; If this is not a space view, jump to tt66 to skip
  BNE tt66               ; displaying the view name
 
- LDA #11                ; Move the text cursor to row 11
+ LDA #11                ; Move the text cursor to column 11
  STA XC
 
  LDA VIEW               ; Load the current view into A:
